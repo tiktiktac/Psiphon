14085e8076d7b4246d71dbed380679c25cb5fc04
tiktiktac <tiktiktac07@gmail.com>
2024-09-17 23:30:57 +0700

    Add box for magisk

---
+++ enable-psiphon-bfm.sh
@@ -0,0 +1,18 @@
+#!/data/data/com.termux/files/usr/bin/sh
+
+#prepare tmp directory
+mkdir -p /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp
+rm -rf /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/*
+cp /data/data/com.termux/files/home/Psiphon/box-for-magisk/bfm-settings.ini /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-settings.ini
+cp ./data/data/com.termux/files/home/Psiphon/box-for-magisk/bfm-clash-config.yaml /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-clash-config.yaml
+cp ./data/data/com.termux/files/home/Psiphon/box-for-magisk/bfm-xray-config.json /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-xray-config.json
+
+sed -i 's/bin_name=\"xray\"/bin_name=\"clash\"/g' /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-settings.ini
+sed -i 's/bin_name=\"sing-box\"/bin_name=\"clash\"/g' /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-settings.ini
+sed -i 's/bin_name=\"v2fly\"/bin_name=\"clash\"/g' /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-settings.ini
+
+#replace bfm configs
+su -c "mv -f /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-settings.ini /data/adb/box/settings.ini && mv -f /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-clash-config.yaml /data/adb/box/clash/config.yaml"
+
+#restart bfm
+su -c /data/adb/box/scripts/box.service restart
---
+++ enable-xray-bfm.sh
@@ -0,0 +1,23 @@
+#!/data/data/com.termux/files/usr/bin/sh
+
+if [ ! -f /data/data/com.termux/files/home/Psiphon/box-for-magisk/bfm-xray-config.json ]; then
+    echo "V2ray config not found!"
+    exit 1
+fi
+
+#prepare tmp directory
+mkdir -p /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp
+rm -rf /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/*
+cp /data/data/com.termux/files/home/Psiphon/box-for-magisk/bfm-settings.ini /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-settings.ini
+cp ./data/data/com.termux/files/home/Psiphon/box-for-magisk/bfm-clash-config.yaml /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-clash-config.yaml
+cp ./data/data/com.termux/files/home/Psiphon/box-for-magisk/bfm-xray-config.json /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-xray-config.json
+
+sed -i 's/bin_name=\"clash\"/bin_name=\"xray\"/g' /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-settings.ini
+sed -i 's/bin_name=\"sing-box\"/bin_name=\"xray\"/g' /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-settings.ini
+sed -i 's/bin_name=\"v2fly\"/bin_name=\"xray\"/g' /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-settings.ini
+
+#replace bfm configs
+su -c "mv -f /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-settings.ini /data/adb/box/settings.ini && mv -f /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-xray-config.json /data/adb/box/xray/config.json"
+
+#restart bfm
+su -c /data/adb/box/scripts/box.service restart
