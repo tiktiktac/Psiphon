a885dfbea36e974a8cda6fa8daf8599e81095f33
tiktiktac <tiktiktac07@gmail.com>
2024-09-18 21:55:43 +0700

    Update configs

--- psiphon-clash-socks5-bfm.sh
+++ psiphon-clash-socks5-bfm.sh
@@ -4,15 +4,14 @@
 mkdir -p /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp
 rm -rf /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/*
 cp /data/data/com.termux/files/home/Psiphon/box-for-magisk/bfm-settings.ini /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-settings.ini
-cp /data/data/com.termux/files/home/Psiphon/box-for-magisk/bfm-clash-config.yaml /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-clash-config.yaml
-cp /data/data/com.termux/files/home/Psiphon/box-for-magisk/bfm-xray-config.json /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-xray-config.json
+cp /data/data/com.termux/files/home/Psiphon/box-for-magisk/bfm-clash-socks5-config.yaml /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-clash-socks5-config.yaml
 
 sed -i 's/bin_name=\"xray\"/bin_name=\"clash\"/g' /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-settings.ini
 sed -i 's/bin_name=\"sing-box\"/bin_name=\"clash\"/g' /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-settings.ini
 sed -i 's/bin_name=\"v2fly\"/bin_name=\"clash\"/g' /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-settings.ini
 
 #replace bfm configs
-su -c "mv -f /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-settings.ini /data/adb/box/settings.ini && mv -f /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-clash-config.yaml /data/adb/box/clash/config.yaml"
+su -c "mv -f /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-settings.ini /data/adb/box/settings.ini && mv -f /data/data/com.termux/files/home/Psiphon/box-for-magisk/tmp/bfm-clash-socks5-config.yaml /data/adb/box/clash/config.yaml"
 
 #restart bfm
 su -c /data/adb/box/scripts/box.service restart
